d226f1f856f57663df138023ac0477b0aa85440a
Michael Rubino <parkcathedralgrace@gmail.com>
2019-03-29 20:47:32 -0400

    Starting components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+//Components
+import NavBar from './components/NavBar';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar></NavBar>
         <p>Test</p>
       </div>
     );
---
+++ front-end/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function NavBar(){
+    return(
+        <div>
+            <p>BUTS</p>
+        </div>
+    )
+}
+
+export default NavBar;
