5d61151663832b8876b0eb631f7d1c199a02e6a9
Michael Rubino <parkcathedralgrace@gmail.com>
2019-03-30 10:50:33 -0400

    Header for main app

--- front-end/src/components/Main.js
+++ front-end/src/components/Main.js
@@ -4,8 +4,18 @@ import '../styles/main.css';
 class Main extends Component {
     render(){
         return(
-            <div class="main-app">
-                <p>!</p>
+            <div className="main-app">
+                <div className="container my-3">
+                    <div className="app-header text-center text-md-left">
+                        <span>Potential Target Companies</span>
+                        <div className="container">
+                            <div className="mt-3 mt-md-2 bg-primary p-4">
+                                <p className="brief-text text-white">Dynamically target high-payoff intellectual capital for customized technologies. Objectively integrate emerging core competencies before process-centric communities. Dramatically evisculate holistic innovation rather than client-centric data.</p>
+                                <p className="brief-text text-white">Progressively maintain extensive infomediaries via extensible niches. Dramatically disseminate standardized metrics after resource-leveling processes. Objectively pursue diverse catalysts for change for interoperable meta-services.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
