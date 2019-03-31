cf811ba5afcab8fb2828a36f4bf23779d09bdc40
Michael Rubino <parkcathedralgrace@gmail.com>
2019-03-31 13:58:33 -0400

    Quick fixes on Navbar & Footer

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -6,27 +6,27 @@ function Footer(){
     const curYear = curDate.getFullYear();
 
     return(
-        <footer class="page-footer font-small navbar-dark bg-dark p-1 p-md-5">
-            <div class="container-fluid text-center text-md-left">
-                <div class="row">
-                    <div class="col-md-8 mt-md-0 mt-3">
-                        <h5 class="text-uppercase">Footer Content</h5>
+        <footer className="page-footer font-small navbar-dark bg-dark p-1 p-md-5">
+            <div className="container-fluid text-center text-md-left">
+                <div className="row">
+                    <div className="col-md-8 mt-md-0 mt-3">
+                        <h5 className="text-uppercase">Footer Content</h5>
                         <p>
                             Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.
                         </p>
                     </div>
-                    <hr class="clearfix w-100 d-md-none pb-3"/>
-                    <div class="col-md-2 mb-md-0 mb-3">
-                        <h5 class="text-uppercase">Company</h5>
-                        <ul class="list-unstyled">
+                    <hr className="clearfix w-100 d-md-none pb-3"/>
+                    <div className="col-md-2 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Company</h5>
+                        <ul className="list-unstyled">
                             <li><a href="#!">Link 1</a></li>
                             <li><a href="#!">Link 2</a></li>
                             <li><a href="#!">Link 3</a></li>
                         </ul>
                     </div>
-                    <div class="col-md-2 mb-md-0 mb-3">
-                        <h5 class="text-uppercase">Links</h5>
-                        <ul class="list-unstyled">
+                    <div className="col-md-2 mb-md-0 mb-3">
+                        <h5 className="text-uppercase">Links</h5>
+                        <ul className="list-unstyled">
                             <li><a href="#!">Link 1</a></li>
                             <li><a href="#!">Link 2</a></li>
                             <li><a href="#!">Link 3</a></li>
@@ -34,7 +34,7 @@ function Footer(){
                     </div>
                 </div>
             </div>
-            <div class="footer-copyright py-3 ml-3 text-center text-md-left">© {curYear} Insisten</div>
+            <div className="footer-copyright py-3 ml-3 text-center text-md-left">© {curYear} Insisten</div>
         </footer>
     );
 }
--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -6,21 +6,21 @@ class NavBar extends Component{
     render(){
         return(
             <div className="navbar-container">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
-                    <Link class="navbar-brand" to="/">Insiten</Link>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+                    <Link className="navbar-brand" to="/">Insiten</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                            <a class="nav-item nav-link" href="#">FAQ</a>
-                            <a class="nav-item nav-link" href="#">Contact</a>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link" href="#">FAQ</a>
+                            <a className="nav-item nav-link" href="#">Contact</a>
                         </div>
                     </div>
-                    <form class="form-inline nav-search">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                    <form className="form-inline nav-search">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-primary my-2 my-sm-0" type="submit"><i className="fas fa-search"></i></button>
                     </form>
                 </nav>
             </div>
