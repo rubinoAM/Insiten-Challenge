1010f2aa28026d14fc6a498cddcf2b0134cd0a70
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-02 16:01:36 -0400

    Build

---
+++ front-end/build/precache-manifest.ec5a2626fe06b84d7de1ee3c41bfca02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ff88fc3b77f7edfd0d1a",
+    "url": "/static/js/main.c0828121.chunk.js"
+  },
+  {
+    "revision": "629bd74e02cd9df5c7e9",
+    "url": "/static/js/2.c3119e6b.chunk.js"
+  },
+  {
+    "revision": "ff88fc3b77f7edfd0d1a",
+    "url": "/static/css/main.b2343415.chunk.css"
+  },
+  {
+    "revision": "86f08b94b1a1415e1389b09bbda95a93",
+    "url": "/index.html"
+  }
+];
